dbaa2fdecf3de4344dbcab771b304e7414f8acb3
ashoktallapelli <ashok.tallapelli@gmail.com>
2024-07-21 11:25:28 +0800

    Update gradle version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,16 +7,3 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.compose.compiler) apply false
 }
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-    }
-    dependencies {
-        classpath(libs.androidGradle)
-        classpath(libs.composeGradle)
-        classpath(libs.kotlinGradle)
-//        classpath((kotlin("serialization", version = "1.8.20")))
-    }
-}
